7de6ce1d7d826a580e1b1c953c44d05b77ebcae4
Manish Verma <manish.verma217@gmail.com>
2023-08-21 22:36:49 +0530

    Initial Commit

---
+++ src/main/java/com/codingameproblem/workspace/CodingameproblemApplication.java
@@ -0,0 +1,13 @@
+package com.codingameproblem.workspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingameproblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingameproblemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingameproblem/workspace/CodingameproblemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingameproblem.workspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingameproblemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
